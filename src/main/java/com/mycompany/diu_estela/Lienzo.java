618606ceafbeb191a18d2594d245619ee71be992
JorgeBlanco99 <jorgeblancoanton@hotmail.com>
2021-10-14 09:54:08 +0100

    V. 2 Funcioinal

--- src/main/java/com/mycompany/diu_estela/Lienzo.java
+++ src/main/java/com/mycompany/diu_estela/Lienzo.java
@@ -11,16 +11,50 @@ import javax.swing.JPanel;
 
 /**
  *
- * @author gabri
+ * @author gabri & Jorge
  */
 public class Lienzo extends JPanel{
 //pintar contenido
+   // Atributos para manejar la paleta de colores
+    private final Color[] coloresFondo = {Color.WHITE, Color.BLACK, Color.BLUE, Color.GREEN};
+    private final Color[] coloresEstela = {Color.BLACK, Color.BLUE, Color.GREEN, Color.RED};
+    private int colorEstela = 0;
+    private int colorFondo = 0;
+    
+    // Atributos para controlar donde se pintan los círculos
+    private Integer x[] = {null, null, null, null, null};
+    private Integer y[] = {null, null, null, null, null};
+    private int index = 0;
+    
     @Override
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawLine(10, 10, 60, 60);
-        g.drawOval(100, 100, 50, 30);
-        g.fillOval(50, 50, 50, 30);
+        this.setBackground(coloresFondo[colorFondo]);
+        this.setForeground(coloresEstela[colorEstela]);
         
+        for(int i = 0; i < 5; i++) {
+            if(x[i] != null && y[i] != null) {
+                g.fillOval(x[i], y[i], 10, 10);
+            }            
+        }
+        
+    }
+    
+    public void setCoordinates(int x, int y) {
+        this.x[this.index] = x;
+        this.y[this.index] = y;
+        index++;
+        if(index > 4) index = 0;
+        repaint();
+    }
+    
+    public void paintFondo(int index) {
+        colorFondo = index;
+        repaint();
+    }
+    
+    public void paintEstela(int index) {
+        colorEstela = index;
+        repaint();
     }
 }
